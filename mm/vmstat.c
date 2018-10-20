b6462a44b4f3785d0681ea1d944276eff6972094
Jiaxun Yang <jiaxun.yang@flygoat.com>
2018-10-20 13:34:48 +0800

    mm: introduce uksm
    Ultra KSM is a set of out-of-tree patch which provide
    a better implemention of Kernel Samepage Merging feature.
    https://github.com/dolohow/uksm
    
    Fixed by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1092,6 +1092,9 @@ const char * const vmstat_text[] = {
 	"nr_written",
 	"", /* nr_indirectly_reclaimable */
 
+#ifdef CONFIG_UKSM
+	"nr_uksm_zero_pages",
+#endif
 	/* enum writeback_stat_item counters */
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
